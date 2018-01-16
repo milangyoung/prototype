d11dafd6f7051f1fa4587284394da070eb044620
milangyoung <34363226+milangyoung@users.noreply.github.com>
2018-01-16 13:05:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
 	<title></title>
 </head>
 <body>
+	<label>First edit</label>
 <img src="first.png">
 <img src="second.png">
 <img src="third.png">
+	<label>Second</label>
+	<img src="one.png">
+<img src="two.png">
 </body>
 </html>
