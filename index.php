d16a1b275d99b0dc9f2f1615013179b14ca08c1f
milangyoung <34363226+milangyoung@users.noreply.github.com>
2018-01-17 16:41:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
 	<title></title>
 </head>
 <body>
-	<label>First edit</label>
+	<label>First edit</label><br>
 <img src="first.png">
 <img src="second.png">
 <img src="third.png"><br>
-	<label>Second EDIT</label>
+	<label>Second EDIT</label><br>
 <img src="one.PNG">
 <img src="two.PNG"><br>
-	<lable>THIRD EDIT</label>
-	<img src="a.PNG">
+	<lable>THIRD EDIT</label><br>
+<img src="a.PNG">
 <img src="b.PNG">
 <img src="c.PNG">
-	<img src="d.PNG">
+<img src="d.PNG">
 <img src="e.PNG">
 
 </body>
